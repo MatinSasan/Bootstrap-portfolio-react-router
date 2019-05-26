1ebe39634da75e6ca0ff39e1548269c4a5d6d6b9
Matin Sasanpour <matin.sasan@gmail.com>
2019-05-26 22:37:28 +0430

    Updates

---
+++ precache-manifest.045eb7b19a3958664d751a20d88dbe97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad84c63bb6ba1fe527898d92f1d7365c",
+    "url": "/Bootstrap-portfolio-react-router/index.html"
+  },
+  {
+    "revision": "659d445ea1937abf1320",
+    "url": "/Bootstrap-portfolio-react-router/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d85b0e416895b2b61172",
+    "url": "/Bootstrap-portfolio-react-router/static/css/main.fcfd6045.chunk.css"
+  },
+  {
+    "revision": "659d445ea1937abf1320",
+    "url": "/Bootstrap-portfolio-react-router/static/js/2.1e26efc5.chunk.js"
+  },
+  {
+    "revision": "d85b0e416895b2b61172",
+    "url": "/Bootstrap-portfolio-react-router/static/js/main.c00fb3ee.chunk.js"
+  },
+  {
+    "revision": "75e58a71d4302e7a4ee4",
+    "url": "/Bootstrap-portfolio-react-router/static/js/runtime~main.9cd0ccbc.js"
+  }
+]);
