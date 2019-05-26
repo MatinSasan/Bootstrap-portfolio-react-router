a04bec57ad5f95328cec02595b21d0eeb3cf8ad0
Matin Sasanpour <matin.sasan@gmail.com>
2019-05-27 00:16:44 +0430

    Updates

---
+++ precache-manifest.ec97501e8b18c1820c4d2bf307197ad2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4a68070e020e8b53fead3a8b700b92",
+    "url": "/Bootstrap-portfolio-react-router/index.html"
+  },
+  {
+    "revision": "b1c6159055972eacbc3c",
+    "url": "/Bootstrap-portfolio-react-router/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "adf4348834bcb94210e2",
+    "url": "/Bootstrap-portfolio-react-router/static/css/main.da380429.chunk.css"
+  },
+  {
+    "revision": "b1c6159055972eacbc3c",
+    "url": "/Bootstrap-portfolio-react-router/static/js/2.9c2c13ea.chunk.js"
+  },
+  {
+    "revision": "adf4348834bcb94210e2",
+    "url": "/Bootstrap-portfolio-react-router/static/js/main.d9fc1195.chunk.js"
+  },
+  {
+    "revision": "75e58a71d4302e7a4ee4",
+    "url": "/Bootstrap-portfolio-react-router/static/js/runtime~main.9cd0ccbc.js"
+  },
+  {
+    "revision": "08d9f10f8f5a4e1b1856638c0bc113d9",
+    "url": "/Bootstrap-portfolio-react-router/static/media/img (1).08d9f10f.jpg"
+  },
+  {
+    "revision": "4c927b1672a5607f3aeb6b40c9c0fbc6",
+    "url": "/Bootstrap-portfolio-react-router/static/media/img (2).4c927b16.jpg"
+  },
+  {
+    "revision": "39d65933adc28143a92b8a84431f121d",
+    "url": "/Bootstrap-portfolio-react-router/static/media/img (3).39d65933.jpg"
+  },
+  {
+    "revision": "e9f838091acb4f23dd145e61f8a8410b",
+    "url": "/Bootstrap-portfolio-react-router/static/media/img (4).e9f83809.jpg"
+  },
+  {
+    "revision": "6bdeecf2df4eb93083c2f711355ec745",
+    "url": "/Bootstrap-portfolio-react-router/static/media/img (5).6bdeecf2.jpg"
+  },
+  {
+    "revision": "ffaa913fc9d577ea3b9aca142fe3d5b6",
+    "url": "/Bootstrap-portfolio-react-router/static/media/img (6).ffaa913f.jpg"
+  },
+  {
+    "revision": "9f6cdb8a58a58e5a80c59fa69052c508",
+    "url": "/Bootstrap-portfolio-react-router/static/media/portrait1.9f6cdb8a.jpg"
+  },
+  {
+    "revision": "9a29c431003abaef2581c228f7b16a44",
+    "url": "/Bootstrap-portfolio-react-router/static/media/portrait2.9a29c431.jpg"
+  },
+  {
+    "revision": "1f4efb8a2842a3615c628fc9c347134f",
+    "url": "/Bootstrap-portfolio-react-router/static/media/portrait3.1f4efb8a.jpg"
+  },
+  {
+    "revision": "5e56f6df12998117589064a39aaf4d39",
+    "url": "/Bootstrap-portfolio-react-router/static/media/portrait4.5e56f6df.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Bootstrap-portfolio-react-router/precache-manifest.3435201adf4ee2ea888f6cbe60ff5683.js"
+  "/Bootstrap-portfolio-react-router/precache-manifest.ec97501e8b18c1820c4d2bf307197ad2.js"
 );
 
 self.addEventListener('message', (event) => {
